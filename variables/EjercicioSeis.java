3f22eb227b86b9311dc47a007c69d8dad5d12755
Kiruru <107038229+WuKiruru@users.noreply.github.com>
2022-06-08 13:03:13 +0200

    Update EjercicioSeis.java

--- variables/EjercicioSeis.java
+++ variables/EjercicioSeis.java
@@ -7,6 +7,7 @@ public class EjercicioSeis {
         /*Escribe un programa que declare variables de tipo char y de tipo String. 
         Intenta mostrarlas por pantalla todas juntas en la misma línea y con una 
         sola sentencia de Java (con un solo println) ¿es posible? */
+        
         String frase = "Alejandro";
         char letra = 'a';
 
