631644ec3ce6344d3e754add3a4d9d1d1a505f6b
Kiruru <107038229+WuKiruru@users.noreply.github.com>
2022-06-08 13:01:31 +0200

    Update HorarioClase.java

--- salidaDatosPantalla/HorarioClase.java
+++ salidaDatosPantalla/HorarioClase.java
@@ -5,6 +5,7 @@ public class HorarioClase {
     public static void main(String[] args) {
         
         /* Escribe un programa que muestre tu horario de clase */
+        
         System.out.println("Lunes\tMartes\tMierc.\tJueves\tViernes");
         System.out.println("=====\t=====\t=====\t=====\t=====");
         System.out.println("PROG\tPROG\tPROG\tPROG\tSIN");
