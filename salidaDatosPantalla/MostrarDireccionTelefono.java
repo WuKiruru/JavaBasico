274228b74d221a0fb75e37d31724b2cec6d29d3b
Kiruru <107038229+WuKiruru@users.noreply.github.com>
2022-06-08 13:01:57 +0200

    Update MostrarDireccionTelefono.java

--- salidaDatosPantalla/MostrarDireccionTelefono.java
+++ salidaDatosPantalla/MostrarDireccionTelefono.java
@@ -6,6 +6,7 @@ public class MostrarDireccionTelefono {
 
         /* Modifica el programa anterior para que además se muestre tu dirección 
         y tu número de teléfono. */
+        
         System.out.println("Alejandro\n");
         System.out.println("93xxx5xxx\n");
         System.out.println("Calle de Barcelona");
