11db7919ade58896c4ac85862a5882279cfdbdd6
Kiruru <107038229+WuKiruru@users.noreply.github.com>
2022-06-08 13:02:10 +0200

    Update MostrarNombre.java

--- salidaDatosPantalla/MostrarNombre.java
+++ salidaDatosPantalla/MostrarNombre.java
@@ -5,6 +5,7 @@ public class MostrarNombre {
     public static void main(String[] args) {
 
         /* Escribe un programa que muestre tu nombre por pantalla. */
+        
         System.out.println("Alejandro");
     }
 }
