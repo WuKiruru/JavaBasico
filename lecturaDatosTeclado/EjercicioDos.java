6786a17aaab9fdd9ff603df11974c93e37d3585d
Kiruru <107038229+WuKiruru@users.noreply.github.com>
2022-06-08 13:00:02 +0200

    Update EjercicioDos.java

--- lecturaDatosTeclado/EjercicioDos.java
+++ lecturaDatosTeclado/EjercicioDos.java
@@ -10,6 +10,7 @@ public class EjercicioDos {
         /* Adapta el ejercicio 2 de la práctica 2 para que en lugar de asignar 
         un nombre fijo a la variable nombre, lo solicite por pantalla asígnale 
         tu nombre completo. Muestra el valor leído por pantalla. */
+        
         System.out.print("Introduce tu nombre a continuación: ");
         String nombre = teclado.next();
 
