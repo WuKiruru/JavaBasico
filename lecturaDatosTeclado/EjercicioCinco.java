bf8091a4aabc3ed45a8bb5c23c0ed4a429fd2215
Kiruru <107038229+WuKiruru@users.noreply.github.com>
2022-06-08 12:59:43 +0200

    Update EjercicioCinco.java

--- lecturaDatosTeclado/EjercicioCinco.java
+++ lecturaDatosTeclado/EjercicioCinco.java
@@ -13,6 +13,7 @@ public class EjercicioCinco {
         /* Adapta el ejercicio 5 de la práctica 2 para que el programa que calcule 
         el total de una factura solicite al usuario la base imponible (precio sin IVA). 
         El valor del IVA deberás declararlo una constante. */
+        
         System.out.print("Porfavor, introduce la base imponible: ");
         double bimponible = s.nextDouble();
 
