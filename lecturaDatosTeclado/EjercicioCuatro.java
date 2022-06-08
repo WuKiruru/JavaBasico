3c671f8a9b39f78e976725f7f70f0b598d7649db
Kiruru <107038229+WuKiruru@users.noreply.github.com>
2022-06-08 12:59:52 +0200

    Update EjercicioCuatro.java

--- lecturaDatosTeclado/EjercicioCuatro.java
+++ lecturaDatosTeclado/EjercicioCuatro.java
@@ -10,6 +10,7 @@ public class EjercicioCuatro {
         /* Adapta el ejercicio 4 de la práctica 2 para que el conversor de euros 
         a otra moneda solicite la divisa a la que se va a cambiar, la cantidad 
         en euros que se quiere convertir y el cambio aplicado. */
+        
         System.out.print("Introduce la divisia que quieres recibir: ");
         String divisa = s.next();
         System.out.print("Introduce la cantidad de euros a cambiar: ");
