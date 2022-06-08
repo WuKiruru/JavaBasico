e70af6bce37a321e8debe7107f21a7cc164cd812
Kiruru <107038229+WuKiruru@users.noreply.github.com>
2022-06-08 13:02:32 +0200

    Update Piramide.java

--- salidaDatosPantalla/Piramide.java
+++ salidaDatosPantalla/Piramide.java
@@ -7,6 +7,7 @@ public class Piramide {
         /* Escribe un programa que pinte por pantalla una pirámide rellena a 
         base de asteriscos. La base de la pirámide debe estar formada por 9 
         asteriscos. */
+        
         System.out.printf("%6s", "*\n");
         System.out.printf("%7s", "***\n");
         System.out.printf("%8s", "*****\n");
